6ff5aeb2a29933d9fda9f3933455ff71046cba9f
yutong pang <yutongpang@yutongs-iMac.local>
2015-04-01 21:24:21 -0400

    add functional test

--- functional_test.py
+++ functional_test.py
@@ -1,7 +1,19 @@
 __author__ = 'yutongpang'
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+class NewvistorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+            self.browser.quit()
+
+    def test_can_create_a_device(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn('Django', self.browser.title)
+
+if __name__ =='__main__':
+    unittest.main(warnings='ignore')
