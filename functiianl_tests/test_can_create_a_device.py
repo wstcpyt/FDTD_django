0b468cd92ddbe908005d631874207fa28f0eee7c
yutong pang <wstcpyt@gmail.com>
2015-04-02 13:01:14 -0400

    use django template

--- functiianl_tests/test_can_create_a_device.py
+++ functiianl_tests/test_can_create_a_device.py
@@ -1,6 +1,7 @@
 __author__ = 'yutongpang'
 from .base import FunctionalTest
 
+
 class NewvistorTest(FunctionalTest):
     def test_can_create_a_device(self):
         self.browser.get('http://localhost:8000')
