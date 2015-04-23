0793ddb07cd6cf22eac300ed25af42b9f7e88247
yutong pang <yutongpang@yutongs-iMac.local>
2015-04-23 00:15:52 -0400

    responsive d3 chart

--- FDTD_django/static/bower_components/d3-chart/d3-chart.coffee
+++ FDTD_django/static/bower_components/d3-chart/d3-chart.coffee
@@ -23,42 +23,91 @@ polymer = {
         "year": "2010"
       }]
     }
+    data2: {
+      type: Array
+      value: [{
+        "sale": "152",
+        "year": "2000"
+      }, {
+        "sale": "189",
+        "year": "2002"
+      }, {
+        "sale": "179",
+        "year": "2004"
+      }, {
+        "sale": "199",
+        "year": "2006"
+      }, {
+        "sale": "134",
+        "year": "2008"
+      }, {
+        "sale": "176",
+        "year": "2010"
+      }]
+    }
   }
   ready: ->
+    _this = this
+    root = exports ? this
+    $(window).on('resize', ->
+      width = _this.$.visualisation.clientWidth
+      root.xScale = d3.scale.linear().range([margins.left, width - margins.right]).domain([2000,2010])
+      xAxis = d3.svg.axis()
+      .scale(root.xScale)
+      visualization.select('.x.axis').call(xAxis)
+      lineGen = d3.svg.line()
+        .x((d) ->
+          root.xScale(d.year))
+        .y((d) ->
+          yScale(d.sale))
+        .interpolate("basis")
+      visualization.select('#dataset1').attr('d', lineGen(dataset))
+    )
     dataset = this.data
-    vis = d3.select(this.$.visualisation)
-    WIDTH = 1000
-    HEIGHT = 500
-    MARGINS = {
+    dataset2 = this.data2
+    visualization = d3.select(this.$.visualisation)
+    width = window.innerWidth*0.5
+    height = 500
+    margins = {
       top: 20,
       right: 20,
       bottom: 20,
       left: 50
     }
-    xScale = d3.scale.linear().range([MARGINS.left, WIDTH - MARGINS.right]).domain([2000,2010])
-    yScale = d3.scale.linear().range([HEIGHT - MARGINS.top, MARGINS.bottom]).domain([134,215])
+    root.xScale = d3.scale.linear().range([margins.left, width - margins.right]).domain([2000,2010])
+    yScale = d3.scale.linear().range([height - margins.top, margins.bottom]).domain([134,215])
     xAxis = d3.svg.axis()
-      .scale(xScale)
+    .scale(root.xScale)
     yAxis = d3.svg.axis()
-      .scale(yScale)
-      .orient("left")
-    vis.append("svg:g")
-      .attr("transform", "translate(0," + (HEIGHT - MARGINS.bottom) + ")")
-      .call(xAxis)
-    vis.append("svg:g")
-      .attr("transform", "translate(" + (MARGINS.left) + ",0)")
-      .call(yAxis)
+    .scale(yScale)
+    .orient("left")
+    visualization.append("svg:g")
+    .attr("class","x axis")
+    .attr("transform", "translate(0," + (height - margins.bottom) + ")")
+    .call(xAxis)
+    visualization.append("svg:g")
+    .attr("class","axis")
+    .attr("transform", "translate(" + (margins.left) + ",0)")
+    .call(yAxis)
     lineGen = d3.svg.line()
-      .x((d) ->
-        return xScale(d.year))
-      .y((d) ->
-        return yScale(d.sale))
+    .x((d) ->
+      root.xScale(d.year))
+    .y((d) ->
+      yScale(d.sale))
+    .interpolate("basis")
 
-    vis.append('svg:path')
-      .attr('d', lineGen(dataset))
-      .attr('stroke', 'green')
-      .attr('stroke-width', 2)
-      .attr('fill', 'none')
+    visualization.append('svg:path')
+    .attr("id","dataset1")
+    .attr('d', lineGen(dataset))
+    .attr('stroke', 'green')
+    .attr('stroke-width', 2)
+    .attr('fill', 'none')
+
+    visualization.append('svg:path')
+    .attr('d', lineGen(dataset2))
+    .attr('stroke', 'blue')
+    .attr('stroke-width', 2)
+    .attr('fill', 'none')
 }
 
 Polymer(polymer)
--- FDTD_django/static/bower_components/d3-chart/d3-chart.js
+++ FDTD_django/static/bower_components/d3-chart/d3-chart.js
@@ -28,32 +28,73 @@
             "year": "2010"
           }
         ]
+      },
+      data2: {
+        type: Array,
+        value: [
+          {
+            "sale": "152",
+            "year": "2000"
+          }, {
+            "sale": "189",
+            "year": "2002"
+          }, {
+            "sale": "179",
+            "year": "2004"
+          }, {
+            "sale": "199",
+            "year": "2006"
+          }, {
+            "sale": "134",
+            "year": "2008"
+          }, {
+            "sale": "176",
+            "year": "2010"
+          }
+        ]
       }
     },
     ready: function() {
-      var HEIGHT, MARGINS, WIDTH, dataset, lineGen, vis, xAxis, xScale, yAxis, yScale;
+      var _this, dataset, dataset2, height, lineGen, margins, root, visualization, width, xAxis, yAxis, yScale;
+      _this = this;
+      root = typeof exports !== "undefined" && exports !== null ? exports : this;
+      $(window).on('resize', function() {
+        var lineGen, width, xAxis;
+        width = _this.$.visualisation.clientWidth;
+        root.xScale = d3.scale.linear().range([margins.left, width - margins.right]).domain([2000, 2010]);
+        xAxis = d3.svg.axis().scale(root.xScale);
+        visualization.select('.x.axis').call(xAxis);
+        lineGen = d3.svg.line().x(function(d) {
+          return root.xScale(d.year);
+        }).y(function(d) {
+          return yScale(d.sale);
+        }).interpolate("basis");
+        return visualization.select('#dataset1').attr('d', lineGen(dataset));
+      });
       dataset = this.data;
-      vis = d3.select(this.$.visualisation);
-      WIDTH = 1000;
-      HEIGHT = 500;
-      MARGINS = {
+      dataset2 = this.data2;
+      visualization = d3.select(this.$.visualisation);
+      width = window.innerWidth * 0.5;
+      height = 500;
+      margins = {
         top: 20,
         right: 20,
         bottom: 20,
         left: 50
       };
-      xScale = d3.scale.linear().range([MARGINS.left, WIDTH - MARGINS.right]).domain([2000, 2010]);
-      yScale = d3.scale.linear().range([HEIGHT - MARGINS.top, MARGINS.bottom]).domain([134, 215]);
-      xAxis = d3.svg.axis().scale(xScale);
+      root.xScale = d3.scale.linear().range([margins.left, width - margins.right]).domain([2000, 2010]);
+      yScale = d3.scale.linear().range([height - margins.top, margins.bottom]).domain([134, 215]);
+      xAxis = d3.svg.axis().scale(root.xScale);
       yAxis = d3.svg.axis().scale(yScale).orient("left");
-      vis.append("svg:g").attr("transform", "translate(0," + (HEIGHT - MARGINS.bottom) + ")").call(xAxis);
-      vis.append("svg:g").attr("transform", "translate(" + MARGINS.left + ",0)").call(yAxis);
+      visualization.append("svg:g").attr("class", "x axis").attr("transform", "translate(0," + (height - margins.bottom) + ")").call(xAxis);
+      visualization.append("svg:g").attr("class", "axis").attr("transform", "translate(" + margins.left + ",0)").call(yAxis);
       lineGen = d3.svg.line().x(function(d) {
-        return xScale(d.year);
+        return root.xScale(d.year);
       }).y(function(d) {
         return yScale(d.sale);
-      });
-      return vis.append('svg:path').attr('d', lineGen(dataset)).attr('stroke', 'green').attr('stroke-width', 2).attr('fill', 'none');
+      }).interpolate("basis");
+      visualization.append('svg:path').attr("id", "dataset1").attr('d', lineGen(dataset)).attr('stroke', 'green').attr('stroke-width', 2).attr('fill', 'none');
+      return visualization.append('svg:path').attr('d', lineGen(dataset2)).attr('stroke', 'blue').attr('stroke-width', 2).attr('fill', 'none');
     }
   };
 
