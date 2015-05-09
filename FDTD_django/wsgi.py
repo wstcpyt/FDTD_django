d2a67c335ec7507fb7a0dfa79dc2d1647a05e048
yutong pang <yutongpang@yutongs-iMac.home>
2015-05-09 11:17:03 -0400

    add new relic

--- FDTD_django/wsgi.py
+++ FDTD_django/wsgi.py
@@ -8,9 +8,12 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
 import os
-
+import newrelic.agent
 from django.core.wsgi import get_wsgi_application
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "FDTD_django.settings")
-
+newrelic_root = os.path.join(BASE_DIR, 'newrelic.ini')
+newrelic.agent.initialize(newrelic_root)
 application = get_wsgi_application()
+application = newrelic.agent.wsgi_application()(application)
