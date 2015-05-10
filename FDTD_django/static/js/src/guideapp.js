0025aa62fcfa75ab99b7adda422185ad7103bedf
yutong pang <yutongpang@yutongs-iMac.local>
2015-05-10 16:03:39 -0400

    add toggle button

--- FDTD_django/static/js/src/guideapp.js
+++ FDTD_django/static/js/src/guideapp.js
@@ -4,7 +4,25 @@
 
   app = angular.module('FDTDapp', ['ngMaterial']);
 
-  app.controller('GuideCtrl', function($scope, $log, $http) {});
+  app.controller('GuideCtrl', function($scope, $timeout, $mdSidenav, $mdUtil, $log, $mdMedia) {
+    var buildToggler;
+    buildToggler = function(navID) {
+      var debounceFn;
+      debounceFn = $mdUtil.debounce(function() {
+        return $mdSidenav(navID).toggle().then(function() {
+          return $log.debug("toggle" + navID);
+        });
+      }, 300);
+      return debounceFn;
+    };
+    $scope.toggleLeft = buildToggler('left');
+    $scope.screenIsgtmd = $mdMedia('gt-md');
+    return $scope.$watch(function() {
+      return $mdMedia('gt-md');
+    }, function() {
+      return $scope.screenIsgtmd = $mdMedia('gt-md');
+    });
+  });
 
   app.controller('LeftCtrl', function($scope, $log, $http) {});
 
