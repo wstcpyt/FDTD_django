e07c5698c3fa27fd0969fec33927e7226181fc55
yutong pang <yutongpang@yutongs-iMac.home>
2015-05-31 15:40:38 -0400

    boto

--- socialauth/models.py
+++ socialauth/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
+class Customer(models.Model):
+    user = models.OneToOneField(User)
+    avatar = models.TextField()
+
--- socialauth/pipeline.py
+++ socialauth/pipeline.py
@@ -1,6 +1,17 @@
 from social.backends.twitter import TwitterOAuth
+from socialauth.models import Customer
+from urllib.request import urlopen
 
 
 def save_profile(backend, user, response, *args, **kwargs):
     if isinstance(backend, TwitterOAuth):
-        print(response)
+        customer = Customer.objects.filter(user=user).all()
+        image_stream = urlopen(response.get('profile_image_url')).read()
+        print(image_stream)
+        if len(customer) == 0:
+            display_name = response.get('screen_name')
+            image_name = 'tw_avatar_%s.jpg' % display_name
+            customer = Customer()
+            customer.user = user
+            customer.avatar = response.get('profile_image_url')
+            customer.save()
